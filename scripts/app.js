4d359db19e4160be87c1e1a95b15f70d185c8b33
William Blacknall <williamblacknall@gmail.com>
2019-09-27 11:14:24 -0400

    Added iframe functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,25 @@
 $(document).ready(function() {    
     const formContainer = $("#formContainer");
     let foodSelector = $("#foodSelector");
-    const contentContainer = $("#contentContainer")
-    const recipeContainer = $("#recipeContainer")
+    const recipeContainer = $("#recipeContainer");
+    const iframePage = $("#iframePage")
+    const link = $("#link")
 
     formContainer.submit('submit', onSubmit);
 
     function onSubmit(e) {
         e.preventDefault();
         let foodItem = foodSelector.val();
-        console.log(foodItem);
         recipeApi(foodItem);
     }
 
+    link.click('click', onClick);
 
+    function onClick(e) {
+        e.preventDefault();
+        iframePage.attr("src", `${link.val()}`)
+        console.log("The button at least works")
+    }
 
     const recipeApi = (foodItem) => {$.getJSON(`https://my-little-cors-proxy.herokuapp.com/http://www.recipepuppy.com/api/?q=${foodItem}`, function(data){
         let randomRecipePicker = Math.floor(Math.random() * data.results.length)
@@ -22,15 +28,14 @@ $(document).ready(function() {
         let dishLink = data.results[randomRecipePicker].href
         let dishImage = data.results[randomRecipePicker].thumbnail
 
-
-        console.log(data.results.length)
-
-
-
         recipeContainer.append(`<p>Dish Name: ${dishName}</p>`)
         recipeContainer.append(`<p>What You'll Need: ${dishIngredients}</p>`)
-        recipeContainer.append(`<a href=${dishLink} class="link">Link to Recipe</a></br>`)
+        recipeContainer.append(`<a href=${dishLink} class="link" id="link">Link to Recipe</a></br>`)
         recipeContainer.append(`<img src="${dishImage}" class="img-border"></br>`)
+        recipeContainer.append(`<p class="line"></p></br>`)
+        iframePage.attr("src",`${dishLink}`)
     });
+
+
 }
 });
